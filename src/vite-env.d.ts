b84a37d2f6a8441de5a7cdf95e402e35a7188faa
Paweł Gosk <pawel@pawelgosk.com>
2024-04-05 09:17:27 +0200

    chore: Add vite config envs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_COPYRIGHTS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare const APP_VERSION: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  build: {
+    sourcemap: true,
+  },
+  define: {
+    APP_VERSION: JSON.stringify(packageJson.version),
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
+});
