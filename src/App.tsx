aa583495526518b6e151ea27e09d6ab67d3337e7
Pawel Gosk <pawel@pawelgosk.com>
2024-04-07 06:14:01 +0200

    feat: Add ! to heading

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React!</h1>
       <h2>Hello after update! Version: {APP_VERSION}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
